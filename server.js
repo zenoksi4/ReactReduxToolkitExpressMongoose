c644381e7f3ab18a5a32cb2f7956d821acc50c54
Pasha <zenoken4ik@gmail.com>
2023-02-04 18:42:46 +0200

    added basic settings express

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-});
+// application/json
+app.use(express.json())
+// application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// images path
+app.use("/static", express.static(__dirname + "/assets"))
+
+app.use('/api/items', require('./routes/items'))
 
 mongoose.connect("mongodb://localhost:27017")
     .then(() => {
