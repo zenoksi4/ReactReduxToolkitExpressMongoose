7e8646c2c624ee37b560d68e901546a8a4b0dd6c
Pasha <zenoken4ik@gmail.com>
2023-02-14 18:44:33 +0200

    added wave animation to header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -10,6 +10,11 @@ const Header = () => {
                 <h1 className={ styles.title }>{`CS GO items at low prices`}</h1>
                 <p className={ styles.desc }>{`You can get rare CS GO items in your inventory!`}</p>
             </ContentWrapper>
+            <div class={ styles.ocean }>
+                <div class={ styles.waveOcean }></div>
+                <div class={ styles.waveOcean }></div>
+                <div class={ styles.waveOcean }></div>
+            </div>
             <img src={ WaveImage } alt="" className={ styles.wave }/>
         </div>
     )
